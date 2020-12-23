69de9139942b3cc9249f960a703eb0a395202073
Marius Schröter <marius@luna.ag>
2020-12-23 15:26:48 +0100

    #working dialog

--- lib/screens/matches_screen.dart
+++ lib/screens/matches_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:football_provider_app/widgets/app_bar_logo_and_title.dart';
+import 'package:football_provider_app/widgets/bottom_dialog.dart';
 import 'package:football_provider_app/widgets/svg.dart';
 import 'package:provider/provider.dart';
 
@@ -23,6 +24,8 @@ class MatchesScreen extends StatefulWidget {
 class _MatchesScreenState extends State<MatchesScreen> {
   var _showNewestFirst = true;
   var _isLoading = false;
+
+  bool _showDialog;
 
   Future<void> _refreshMatches() async {
     Future.delayed(Duration.zero).then((_) {
@@ -48,6 +51,7 @@ class _MatchesScreenState extends State<MatchesScreen> {
   @override
   Widget build(BuildContext context) {
     final matchesLength = Provider.of<MatchesProvider>(context).items.length;
+    _showDialog = matchesLength == 0;
     return Scaffold(
       appBar: AppBar(
         title: AppBarLogoAndTitle(
@@ -85,21 +89,62 @@ class _MatchesScreenState extends State<MatchesScreen> {
             )
           : RefreshIndicator(
               onRefresh: () => _refreshMatches(),
-              child: Container(
-                child: Stack(
-                  children: <Widget>[
-                    Column(
-                      children: [
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.only(top: 16.0),
-                            child: MatchesList(_showNewestFirst),
+              child: Stack(
+                children: <Widget>[
+                  _showDialog
+                      ? Padding(
+                          padding: const EdgeInsets.only(bottom: 64.0),
+                          child: BottomDialog(
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.fromLTRB(
+                                      24.0, 20.0, 24.0, 24.0),
+                                  child: Text('Here you can track new matches',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .headline6),
+                                ),
+                                Align(
+                                  alignment: Alignment.bottomRight,
+                                  child: Padding(
+                                    padding: const EdgeInsets.fromLTRB(
+                                        10.0, 20.0, 10.0, 20.0),
+                                    child: Icon(
+                                      Icons.arrow_downward,
+                                      size: 36.0,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            // title: Text('Here you can track new matches'),
+                            // content: Text(''),
+                            // actions: [
+                            //   FlatButton(
+                            //     child: Text('Ok'),
+                            //     onPressed: () {
+                            //       setState(() {
+                            //         _showDialog = false;
+                            //       });
+                            //     },
+                            //   )
+                            // ],
                           ),
+                        )
+                      : SizedBox(),
+                  Column(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 16.0),
+                          child: MatchesList(_showNewestFirst),
                         ),
-                      ],
-                    ),
-                  ],
-                ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
     );
